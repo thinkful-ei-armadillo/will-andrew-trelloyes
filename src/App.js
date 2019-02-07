d9e65afba7b55484300759c7db60f9a8d05453ed
andrewbituin <fandrewbituin@gmail.com>
2019-02-07 13:26:27 -0800

    finished both delete and addrandomcard

--- src/App.js
+++ src/App.js
@@ -2,8 +2,6 @@ import React, { Component } from "react";
 import List from "./List";
 import "./App.css";
 
-
-
 class App extends Component {
   static defaultProps = {
     store: {
@@ -14,44 +12,41 @@ class App extends Component {
 
   state = this.props.store;
 
-
-   newRandomCard = (listId) => {
+  newRandomCard = listId => {
     console.log(listId);
-    console.log('random button works')
-    const id = Math.random().toString(36).substring(2, 4)
-      + Math.random().toString(36).substring(2, 4);
+    console.log("random button works");
+    const id =
+      Math.random()
+        .toString(36)
+        .substring(2, 4) +
+      Math.random()
+        .toString(36)
+        .substring(2, 4);
     return {
       id,
       title: `Random Card ${id}`,
-      content: 'lorem ipsum',
-    }
-  }
+      content: "lorem ipsum"
+    };
+  };
 
-
-
-  handleAddRandomCard = (listId) => {
+  handleAddRandomCard = listId => {
     const newCard = this.newRandomCard();
-    console.log(newCard);
+    const newId = newCard.id;
     const updatedAllCards = { ...this.state.allCards };
     const updatedList = [...this.state.lists];
-    // updatedList.filter(item => item.id === listId);
-    for (let i = 0; updatedList.length; i++) {
+    updatedAllCards[newId] = newCard;
+    for (let i = 0; i < updatedList.length; i++) {
       if (updatedList[i].id === listId) {
-        updatedList[i].cardIds.push(newCard.id)
-      };   
-      break;
+        updatedList[i].cardIds.push(newId);
+        break;
+      }
     }
-    console.log(updatedList);
-    const testing = newCard.id;
-    updatedAllCards[testing] = newCard;
-    console.log(updatedAllCards);
-    this.setState({ 
+
+    this.setState({
       lists: updatedList,
       allCards: updatedAllCards
     });
-
   };
-
 
   handleDeleteCard = (listId, itemId) => {
     const updatedList = [...this.state.lists];
@@ -65,7 +60,6 @@ class App extends Component {
     }
     this.setState({ lists: updatedList });
   };
-
 
   render() {
     return (
